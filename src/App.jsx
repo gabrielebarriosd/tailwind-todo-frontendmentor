e003d51408fff0d1e88679bb15074b8297112f4f
Gabriel Enrique Barrios Dominguez <gb@urvenue.com>
2024-02-19 18:02:55 -0600

    Reagrupar para dejer preparado los components

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,21 @@ const App = () => {
           </h1>
           <button><MoonIcon className='fill-white'/></button>
         </div>
-        <form className="bg-white rounded-md overflow-hidden py-4 px-4 flex gap-4 items-center mt-8">
-          <span className="rounded-full border-2 w-5 h-5 inline-block "></span>
-          <input
-            type="text"
-            placeholder="Create a new todo..."
-            className="w-full text-gray-600 outline-none"
-          />
-        </form>
+        
       </header>
 
       <main className="container mx-auto px-4 mt-8 ">
-        <div className="bg-white rounded-md [&>article]:p-4">
+        {/* TodoCreate */}
+        <form className="bg-white rounded-md overflow-hidden py-4 px-4 flex gap-4 items-center">
+            <span className="rounded-full border-2 w-5 h-5 inline-block "></span>
+            <input
+              type="text"
+              placeholder="Create a new todo..."
+              className="w-full text-gray-600 outline-none"
+            />
+          </form>
+        {/* TodoList (TodoItem) TodoUpdate & TodoDelete*/}
+        <div className="bg-white rounded-t-md [&>article]:p-4 mt-8">
           <article className="flex gap-4 border-b-gray-400 border-b">
             <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
             <p className="text-gray-600 grow">complete online javascript curse</p>
@@ -37,20 +40,23 @@ const App = () => {
             <p className="text-gray-600 grow">complete online javascript curse</p>
             <button className="flex-none"><CrossIcon/></button>
           </article>
-          <section className="px-4 py-4 flex justify-between">
+         </div>
+        {/* TodoComputed */}
+         <section className="px-4 py-4 flex justify-between bg-white rounded-b-md">
             <span className="text-gray-400">5 items left</span>
             <button className="text-gray-400">Clear completed</button>
           </section>
-        </div>
+        {/* TodoFilter */}  
+          <section className="container mx-auto mt-8">
+            <div className="bg-white p-4 rounded-md flex justify-center gap-4 ">
+              <button className="text-blue-600">All</button>
+              <button className="hover:text-blue-600">Active</button>
+              <button className="hover:text-blue-600">Completed</button>
+            </div>
+          </section>
       </main>
-      <section className="container mx-auto px-4 mt-8">
-        <div className="bg-white p-4 rounded-md flex justify-center gap-4 ">
-          <button className="text-blue-600">All</button>
-          <button className="hover:text-blue-600">Active</button>
-          <button className="hover:text-blue-600">Completed</button>
-        </div>
-      </section>
-      <p className="text-center mt-8">Drag and drop to reorder list</p>
+      
+      <footer className="text-center mt-8">Drag and drop to reorder list</footer>
     </div>
   );
 };
