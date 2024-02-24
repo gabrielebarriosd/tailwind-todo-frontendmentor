7c9987dcc88983a7922410c2dfedf616a3ab9766
Gabriel Enrique Barrios Dominguez <gb@urvenue.com>
2024-02-23 19:21:00 -0600

    update feb 23 2024

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,26 @@ import TodoCreate from "./components/TodoCreate";
 import TodoFilter from "./components/TodoFilter";
 import TodoList from "./components/TodoList";
 import Header from "./components/Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const initialStateTodos = [
+/*const initialStateTodos = [
     { id: 1, title: 'Go to the gym', completed: true },
     { id: 2, title: 'Buy groceries', completed: false },
     { id: 3, title: 'Write code', completed: false },
     { id: 4, title: 'Call mom', completed: true },
     { id: 5, title: 'Read a book', completed: false }
-];
+];*/
+const initialStateTodos = JSON.parse(localStorage.getItem('todos')) || [];
 
 const App = () => {
+
+
+
     const [todos, setTodos] = useState(initialStateTodos);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
 
     const createTodo = (title) => {
         const newTodo = {
@@ -61,9 +69,17 @@ const App = () => {
     }
 
     return (
-        <div className="bg-gray-300 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-contain min-h-screen dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] transition-all">
+        <div className="
+        bg-gray-300 
+          bg-[url('./assets/images/bg-mobile-light.jpg')] 
+          bg-no-repeat bg-contain min-h-screen 
+          dark:bg-gray-900 
+          dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] 
+          transition-all duration-1000
+          md:bg-[url('./assets/images/bg-desktop-light.jpg')] 
+          dark:md:bg-[url('./assets/images/bg-desktop-dark.jpg')]">
             <Header />
-            <main className="container mx-auto px-4 mt-8">
+            <main className="container mx-auto px-4 mt-8 md:max-w-xl">
                 <TodoCreate createTodo={createTodo} />
                 {/* TodoList (TodoItem) TodoUpdate & TodoDelete*/}
                 <TodoList todos={filterTodos()} updateTodo={updateTodo} removeTodo={removeTodo} />
